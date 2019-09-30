d22c704827677e316a86106b97c3b31bef46100a
Genesis <Genesis@10.5.24.79>
2019-09-30 09:44:39 -0500

    clase

---
+++ PruebaRep/src/Clase1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Clase1
+ */
+public class Clase1 {
+    public static void main(String[] args) {
+       
+      
+    }
+    
+}
