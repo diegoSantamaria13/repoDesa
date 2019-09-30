0b00d58e1ce2fd1249cc57724d4582cacfe6d8e2
Genesis <Genesis@10.5.24.79>
2019-09-30 09:20:41 -0500

    prueba5

--- PruebaRep/src/Genesis.java
+++ PruebaRep/src/Genesis.java
@@ -10,8 +10,8 @@
  */
 public class Genesis {
     public static void main(String[] args) {
-        System.out.println("Hola");
-       // esto es un comentario 
+       
+      
     }
     
 }
--- PruebaRep/src/Sebas.java
+++ PruebaRep/src/Sebas.java
@@ -11,6 +11,6 @@
 public class Sebas {
     public static void main(String[] args) {
         System.out.println("Hola2");
-        System.out.println(";P ");
+        
     }
 }
